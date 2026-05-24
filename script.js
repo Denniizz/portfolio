7889d04d648dd71e43c92a4a9b7b56af29b5184a
Deniz <denizmustafov6@gmail.com>
2026-05-24 11:21:35 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const themeBtn = document.getElementById("themeBtn");
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+});
+
+
+const heroBtn = document.getElementById('heroBtn');
+
+heroBtn.addEventListener('click', () => {
+    alert('Projects section coming soon!');
+});
