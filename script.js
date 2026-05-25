fc7d622a145bbebcfbc6ae0a78db9e73af42614b
Deniz <denizmustafov6@gmail.com>
2026-05-25 17:36:22 +0300

    Add navbar and initial notes app structure

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const heroBtn = document.getElementById('heroBtn');
 heroBtn.addEventListener('click', () => {
     alert('Projects section coming soon!');
 });
+
+
+const openNotes = document.getElementById("openNotes");
+const notesModal = document.getElementById("notesModal");
+const closeNotes = document.getElementById("closeNotes");
+
+openNotes.addEventListener("click", () => {
+    notesModal.style.display = "flex";
+});
+
+closeNotes.addEventListener("click", () => {
+    notesModal.style.display = "none";
+});
